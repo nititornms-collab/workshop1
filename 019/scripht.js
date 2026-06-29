be1e26d9d24925ae8b3da7ecd5e3c3a081313671
Kodenn <nititorn.ms@gmail.com>
2026-06-29 16:35:28 +0700

    Add files via upload

---
+++ 019/scripht.js
@@ -0,0 +1,10 @@
+for(i=0;i<5;i++){
+    document.write("<h",i,">Computer Science @ NPRU </h",i,">");
+}
+
+for(i=1;i<=10;i++){
+    for(j=1;j<i;j++){
+        document.write("<img src='img/cat.webp'width=100>");
+    }
+    document.write("<br>");
+}
